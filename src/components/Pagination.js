3193746cf6a6e69b02e8e1f9234d968c2f97f075
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-04 16:19:11 +0200

    Add simple styles to pagination and arrows

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components'
 
 import { NEW_SELECTED_PAGE } from '../redux/actions/actions'
 
@@ -7,12 +8,20 @@ import { NEW_SELECTED_PAGE } from '../redux/actions/actions'
 const Pagination = (props) => {
 	if(!props.data) return
     
-	const renderPaginataion = () => Object.keys(props.data).map(page => <div onClick={()=> props.NEW_SELECTED_PAGE(page)} key={page}>{page}</div>)
+	const renderPaginataion = () => Object.keys(props.data).map(page => <PageNumber onClick={()=> props.NEW_SELECTED_PAGE(page)} key={page}>{page}</PageNumber>)
     
 	return (
-		<div>
+		<PaginationWrapper>
+			{props.selectedPage !== 1 && 
+                <PageNumber onClick={()=> props.NEW_SELECTED_PAGE(props.selectedPage - 1)}>{'<'}</PageNumber>
+			}
+
 			{renderPaginataion()}
-		</div>
+			
+			{Number(props.selectedPage) !== Math.max(...Object.keys(props.data)) && 
+                <PageNumber onClick={()=> props.NEW_SELECTED_PAGE(props.selectedPage + 1)}>{'>'}</PageNumber>
+			}
+		</PaginationWrapper>
 	);
 }
 
@@ -25,3 +34,14 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Pagination)
+
+const PageNumber = styled.div`
+    font-size: 12pt;
+    margin-right: 3%;
+
+`
+
+const PaginationWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`
