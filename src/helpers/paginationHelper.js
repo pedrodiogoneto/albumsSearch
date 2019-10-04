01708d6b043289c780d38950913c1ce01dd96392
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-04 16:36:59 +0200

    Update paginationHelper

--- src/helpers/paginationHelper.js
+++ src/helpers/paginationHelper.js
@@ -3,51 +3,26 @@ const paginationHelper = (data) => {
 
 	let paginatedData = {}  
 
+	const addAlbum = (i, album) => {
+		if(!paginatedData[i]) return paginatedData[i] = [album]
+		return paginatedData[i].push(album)
+	}
+
 	data.map((album, i) => {
-		if(i < 20 ) {
-			if(!paginatedData[1]) return paginatedData[1] = [album]
-			return paginatedData[1].push(album)
-		} 
-		if(i < 40 ) {
-			if(!paginatedData[2]) return paginatedData[2] = [album]
-			return paginatedData[2].push(album)
-		}
-		if(i < 60) {
-			if(!paginatedData[3]) return paginatedData[3] = [album]
-			return paginatedData[3].push(album)
-		}
-		if(i < 80 ) {
-			if(!paginatedData[4]) return paginatedData[4] = [album]
-			return paginatedData[4].push(album)
-		}
-		if(i < 100 ) {
-			if(!paginatedData[5]) return paginatedData[5] = [album]
-			return paginatedData[5].push(album)
-		}
-		if(i < 120 ) {
-			if(!paginatedData[6]) return paginatedData[6] = [album]
-			return paginatedData[6].push(album)
-		}
-		if(i < 140 ) {
-			if(!paginatedData[7]) return paginatedData[7] = [album]
-			return paginatedData[7].push(album)
-		}
-		if(i < 160 ) {
-			if(!paginatedData[8]) return paginatedData[8] = [album]
-			return paginatedData[8].push(album)
-		}
-		if(i < 180 ) {
-			if(!paginatedData[9]) return paginatedData[9] = [album]
-			return paginatedData[9].push(album)
-		}
-		if(i < 200 ) {
-			if(!paginatedData[10]) return paginatedData[10] = [album]
-			return paginatedData[10].push(album)
-		}
+		if(i < 20 ) return addAlbum(1, album)
+		if(i < 40 ) return addAlbum(2, album)
+		if(i < 60 ) return addAlbum(3, album)
+		if(i < 80 ) return addAlbum(4, album)
+		if(i < 100 ) return addAlbum(5, album)
+		if(i < 120 ) return addAlbum(6, album)
+		if(i < 140 ) return addAlbum(7, album)
+		if(i < 160 ) return addAlbum(8, album)
+		if(i < 180 ) return addAlbum(9, album)
+		if(i < 200 ) return addAlbum(10, album)
 	})
 
+	console.log(paginatedData);
 	return paginatedData
-
 }
 
 export default paginationHelper
